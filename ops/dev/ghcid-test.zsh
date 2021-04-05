265ebde2cbe209a86f093378b11d73a18e3bb3c9
Torsten Schmits <torstenschmits@gmail.com>
2021-04-05 18:31:45 +0200

    move all ops stuff to tryp-hs apps

--- ops/dev/ghcid-test.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-setopt err_exit no_unset
-
-# TODO make this an app as well
-${0:h:h}/hpack.zsh
-
-nix run '.#ghcid-test' -- $*
--- ops/dev/release.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-setopt err_exit
-
-result=/tmp/cabal-upload-polysemy-test
-rm -rf $result
-nix build '.#release' -o $result
-nix develop -c $result
-rm -rf $result
