e139e939739d73d7561267bb49543f0f0c7bcfef
Torsten Schmits <torstenschmits@gmail.com>
2021-04-05 16:37:49 +0200

    use the new app from tryp-hs for ghcid-test

--- ops/dev/ghcid-test.zsh
+++ ops/dev/ghcid-test.zsh
@@ -2,14 +2,7 @@
 
 setopt err_exit no_unset
 
-pkg=$1 module=$2 name=$3 type_=$4 runner=$5
-base="${0:a:h:h:h}/"
+# TODO make this an app as well
 ${0:h:h}/hpack.zsh
-nix-shell $*[6,$] --pure -A 'legacyPackages.x86_64-linux.run' \
-  --argstr pkg $pkg \
-  --argstr module $module \
-  --argstr name $name \
-  --argstr 'type' $type_ \
-  --argstr 'runner' $runner \
-  --argstr 'base' $base \
-  --run exit
+
+nix run '.#ghcid-test' -- $*
