1c4804a008db6b7f58d39b58a81551e5228a8ff9
Torsten Schmits <torstenschmits@gmail.com>
2021-04-06 21:15:19 +0200

    bump tryp-hs

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/c0e881852006b132236cbf0301bd1939bb50867e;
-    flake-utils.url = github:numtide/flake-utils;
-    tryp-hs.url = github:tek/tryp-hs;
-    tryp-hs.inputs.nixpkgs.follows = "nixpkgs";
-    polysemy.url = github:polysemy-research/polysemy;
+    tryp-hs = {
+      url = github:tek/tryp-hs;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { tryp-hs, ...}@inputs:
@@ -26,5 +26,6 @@
       packages.polysemy-test = "packages/polysemy-test";
       ghci.extraArgs = ["-fplugin=Polysemy.Plugin"];
       ghcid.prelude = "packages/polysemy-test/lib/Prelude.hs";
+      versionFile = "ops/hpack/packages/polysemy-test.yaml";
     };
 }
