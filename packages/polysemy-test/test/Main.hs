23a56eaf33a6b206872e56eec7d734edb1d105de
Torsten Schmits <torstenschmits@gmail.com>
2021-04-08 01:11:24 +0200

    add missing test to Main

--- packages/polysemy-test/test/Main.hs
+++ packages/polysemy-test/test/Main.hs
@@ -1,10 +1,9 @@
 module Main where
 
-import Test.Tasty (TestTree, defaultMain, testGroup)
-
 import Polysemy.Test (unitTest)
 import Polysemy.Test.Test.FilesTest (test_fixture, test_tempFile)
-import Polysemy.Test.Test.HedgehogTest (test_fail, test_hedgehogRewrite)
+import Polysemy.Test.Test.HedgehogTest (test_close, test_fail, test_hedgehogRewrite)
+import Test.Tasty (TestTree, defaultMain, testGroup)
 
 tests :: TestTree
 tests =
@@ -12,7 +11,8 @@ tests =
     unitTest "read fixtures" test_fixture,
     unitTest "write and read temp files" test_tempFile,
     unitTest "rewrite TestT to Sem" test_hedgehogRewrite,
-    unitTest "convert MonadFail to assertion" test_fail
+    unitTest "convert MonadFail to assertion" test_fail,
+    unitTest "assert that two Doubles are close" test_close
   ]
 
 main :: IO ()
