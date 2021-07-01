0e662edced91032e38bba5900fc64a5da223cac3
Torsten Schmits <torstenschmits@gmail.com>
2021-07-01 23:10:30 +0200

    lint

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
   outputs = { hix, polysemy, ... }:
   let
-    compat901 = { hackage, source, only, jailbreak, minimal, configure, noHpack, ... }: {
+    compat901 = { hackage, source, jailbreak, minimal, noHpack, ... }: {
       path = hackage "0.9.0" "14symzl1rszvk5zivv85k79anz7xyl5gaxy0sm4vhhzsgxc59msv";
       path-io = jailbreak (hackage "1.6.3" "05hcxgyf6kkz36mazd0fqwb6mjy2049gx3vh8qq9h93gfjkpp2vc");
       polysemy = noHpack (minimal (source.root polysemy));
@@ -17,7 +17,7 @@
       type-errors-pretty = jailbreak;
     };
 
-    overrides = { hackage, source, only, jailbreak, ... }: {
+    overrides = { hackage, source, ... }: {
       path = hackage "0.8.0" "0isldidz2gypw2pz399g6rn77x9mppd1mvj5h6ify4pj4mpla0pb";
       polysemy = hackage "1.5.0.0" "1xl472xqdxnp4ysyqnackpfn6wbx03rlgwmy9907bklrh557il6d";
       polysemy-plugin = hackage "0.3.0.0" "1frz0iksmg8bpm7ybnpz9h75hp6hajd20vpdvmi04aspklmr6hj0";
