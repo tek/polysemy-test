2dcf8cfba5c2ad2ad51d5a32067d2c7986ee4cf2
Torsten Schmits <git@tryp.io>
2022-01-17 01:22:53 +0100

    bump hix

--- flake.nix
+++ flake.nix
@@ -5,13 +5,13 @@
 
   outputs = { hix, ... }:
   let
-    compat901 = { hackage, jailbreak, ... }: {
+    compat902 = { hackage, jailbreak, ... }: {
       polysemy = hackage "1.6.0.0" "15k51ysrfcbkww1562g8zvrlzymlk2rxhcsz9ipsb0q6h571qgvf";
       relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
     };
   in hix.flake {
     base = ./.;
-    overrides = { ghc901 = compat901; };
+    overrides = { ghc902 = compat902; };
     packages.polysemy-test = ./packages/polysemy-test;
     versionFile = "ops/hpack/packages/polysemy-test.yaml";
   };
